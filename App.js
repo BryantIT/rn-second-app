21be5e1f35a4585879e903e001c2869e506c2606
Bryant Richards <dragonseyeimages@gmail.com>
2021-12-08 11:47:36 -0600

    04:09:12

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Header from './components/Header'
+import StartGameScreen from './screens/StartGameScreen'
+import { StyleSheet, View } from 'react-native'
 
 export default function App() {
+  const { screen } = styles
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={screen}>
+      <Header />
+    <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    flex: 1
+  }
 });
---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Header = () => {
+  const { header, headerTitle} = styles
+  return (
+    <View style={header}>
+      <Text style={headerTitle}>Guess a Number</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headerTitle : {
+    color: 'white',
+    fontSize: 18
+  }
+})
+
+export default Header
